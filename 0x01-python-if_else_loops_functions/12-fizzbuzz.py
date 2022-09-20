3d4ccd8d100c4d936cbb765cda3370cff3e68b03
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-09-20 16:11:22 -0700

    corrections

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,5 +7,5 @@ def fizzbuzz():
             print("Buzz ", end="")
         elif i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz ", end"")
-        else
+        else:
             print("{:d}".format(i), end="")
